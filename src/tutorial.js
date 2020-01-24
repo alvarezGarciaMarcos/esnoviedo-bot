6fdfbb4e5dede5e937f2a7fc95ac8e6ac4309fff
Marcos Álvarez García <marcosalvarez1997@gmail.com>
2020-01-24 21:32:59 +0100

    Added Express

--- src/tutorial.js
+++ src/tutorial.js
@@ -1,22 +1,26 @@
+const { createServer } = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 const { WebClient } = require('@slack/web-api')
 const { createMessageAdapter } = require('@slack/interactive-messages')
 const slackSigningSecret = process.env.SLACK_SIGNIN_SECRET;
 const slackInteractions = createMessageAdapter(slackSigningSecret)
 
+const port = process.env.PORT;
+
+const app = express();
+
+app.use('/', slackInteractions.requestListener());
+
+app.use(bodyParser());
+
+const server = createServer(app);
+
+server.listen(port, () => {
+    console.log(`Listening for events on ${server.address().port}`);
+  });
 
 slackInteractions.action({type: 'message_action'}, (payload, respond) => {
     console.log('payload', payload)
     respond({text: 'Thanks for your submission'})
 })
-
-
-
-const web = new WebClient(process.env.SLACK_TOKEN);
-const currentTime = new Date().toTimeString();
-
-const port = process.env.port;
-
-(async () => {
-    const server = await slackInteractions.start(port)
-    console.log(`Listening for events on port ${server.address().port}`)
-})();
