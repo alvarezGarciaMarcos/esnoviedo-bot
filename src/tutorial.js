79181b04e6d612153b80f921b0182e54974fc169
Marcos Álvarez García <marcosalvarez1997@gmail.com>
2020-01-26 22:16:16 +0100

    .

--- src/tutorial.js
+++ src/tutorial.js
@@ -216,8 +216,8 @@ app.post("/interactions", function(req, res) {
   const config = {
     headers: { Authorization: "Bearer " + process.env.SLACK_ACCESS_TOKEN }
   };
-
-  const event_cover = {
+  console.log(req.body.payload)
+/*   const event_cover = {
     title: req.body.payload.view.state.values.title.value,
     mp: req.body.payload.view.state.values.mp.value,
     dl: req.body.payload.view.state.values.dl.value,
@@ -232,6 +232,7 @@ app.post("/interactions", function(req, res) {
     text: event_cover,
     channel: "#general"
   };
+   */
   axios.post("https://slack.com/api/chat.postMessage", body, config);
 });
 app.post("/cover", function(req, res) {
