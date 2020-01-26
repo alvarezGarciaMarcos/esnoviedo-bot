1b6d656783ae8cbe2d42d9eb1cd043ab4e1764af
Marcos Álvarez García <marcosalvarez1997@gmail.com>
2020-01-26 22:44:47 +0100

    .

--- src/tutorial.js
+++ src/tutorial.js
@@ -216,7 +216,8 @@ app.post("/interactions", function(req, res) {
   const config = {
     headers: { Authorization: "Bearer " + process.env.SLACK_ACCESS_TOKEN }
   };
-  console.log(req.body.payload.type)
+  console.log(JSON.parse(req.body.payload))
+  
 /*   const event_cover = {
     title: req.body.payload.view.state.values.title.value,
     mp: req.body.payload.view.state.values.mp.value,
